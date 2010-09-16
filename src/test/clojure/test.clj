7d0b0088e0596e226f1257ac1b29d1ac722a5d3c
Matthew O. Smith <m0smith@yahoo.com>
2010-09-16 09:28:36 -0600

    News unit test for topoged.gedcom

---
+++ src/test/clojure/test.clj
@@ -0,0 +1,23 @@
+(ns test
+  (:use [clojure.test :only [run-tests deftest is]])
+  (:use [topoged.gedcom :only [gedcom? gedcom-seq]])
+  )
+
+(def ged1 "src/test/resources/simple.ged")
+(def geds "0 HEAD\n0 @FATHER@ INDI\n1 NAME /Father/")
+
+(deftest gedcom?-test []
+         (is (gedcom? geds))
+         (is (not (gedcom? ged1)))
+         )
+
+(deftest gedcom-seq-test []
+         (let [gseq (gedcom-seq ged1)]
+           (is (= 2 (count (gedcom-seq geds))))
+           (is (= 7 (count gseq)))
+           (is (= "/Father/" (:value (:attrs (first (:content (nth gseq 2))
+                                                  )))))
+           )
+         )
+
+
