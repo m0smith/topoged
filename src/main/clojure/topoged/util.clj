5227e370333489d2338b7b8bf0b6deabf10457c8
Matthew Smith <m0smith@yahoo.com>
2010-09-22 17:44:29 -0600

    Added util.clj

---
+++ src/main/clojure/topoged/util.clj
@@ -0,0 +1,14 @@
+(ns topoged.util )
+
+(defn apply-symbol
+  "Appy the function named 'func-name' to the list 'alist'.
+    func-name can be a function, string, symbol or keyword.
+    alist is the list if arguments.
+    example:
+           (apply-symbol :max '(1 2 3))"
+  ( [func-name alist] (apply-symbol *ns* func-name alist))
+  ( [name-space func-name alist]
+      (if (fn? func-name)
+        (apply func-name alist)
+        (apply (ns-resolve name-space (symbol (name func-name))) alist))))
+
