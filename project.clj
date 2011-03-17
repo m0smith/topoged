47f65d910c6ea8c03746eb4b59b63e7bc5f2f6c4
Matthew Smith <matt@m0smith.com>
2011-03-16 21:05:20 -0600

    Use leiningen

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject topoged "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :target-dir "target/"
+
+  :source-path "src/main/clojure"
+  :library-path "target/dependency"
+  :test-path "src/test/clojure"
+  :resources-path "src/main/resources" 
+  :dev-resources-path "src/test/resources"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+		 [com.miglayout/miglayout "3.7.3" :classifier "swing"]
+		 [clojure-csv/clojure-csv "1.1.0"]
+		 ]
+  :dev-dependencies [[swank-clojure "1.2.0"]])
