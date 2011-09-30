d68b38e47d8ea4387a83b719f6e57b9335d99542
Matthew Smith <m0smith@yahoo.com>
2011-09-30 15:04:33 -0600

    Added with-hibernate-tx macro

--- src/test/clojure/db.clj
+++ src/test/clojure/db.clj
@@ -31,38 +31,43 @@ beginning to make the BigInteger contructor happy"
 
 
 (defn id-factory []
-  (let [id (uuid)
+  (let [^java.util.UUID id (uuid)
 	msb (.getMostSignificantBits id)
 	lsb (.getLeastSignificantBits id)]
     (byte-array (mapcat long-to-bytes [msb lsb]))))
 
 (def hsf (memoize hibernate-session-factory))
 
+(defn begin-tx []
+  (let [^org.hibernate.impl.SessionFactoryImpl sf (hsf)
+	session (.. sf openSession (getSession org.hibernate.EntityMode/MAP))
+	tx (. session beginTransaction)]
+	[session tx]))
+    
+(defmacro with-hibernate-tx
+  [bindings & body]
+  `(let ~bindings
+     (try
+       ~@body
+       (finally
+	(let [^org.hibernate.Session session# (first ~(bindings 0))
+	      tx# (second ~(bindings 0))]
+	  (. session# flush)
+	  (. tx# commit)
+	  (. session# close)
+	)))))
+
 (defn add-type [name desc]
-		      (let [^org.hibernate.impl.SessionFactoryImpl sf (hsf)
-			    sessionp (. sf openSession)
-			    session (. sessionp getSession org.hibernate.EntityMode/MAP)
-			    tx (. session beginTransaction)
-			    entity "Type"
-			    m1 (doto (java.util.HashMap.)
-				 (.put "desc" desc)
-				 (.put "name" name)
-				 (.put "id" (id-factory)))
-			    rtnval (. session save entity  m1)]
-			      (. session flush)
-			      (. tx commit)
-			      (. session close)
-			      rtnval))
-
-			   
-(defn list-types [] (let [^org.hibernate.impl.SessionFactoryImpl sf (hsf)
-			   sessionp (. sf openSession)
-			   session (. sessionp getSession org.hibernate.EntityMode/MAP)
-			   tx (. session beginTransaction)
-			   rtnval (.. session (createQuery "from Type") list)]
-				(. session flush)
-				(. tx commit)
-				(. session close)
-				rtnval))
+  (with-hibernate-tx [[^org.hibernate.Session session tx] (begin-tx)]
+    (let [entity "Type"
+	  m1 (doto (java.util.HashMap.)
+	       (.put "desc" desc)
+	       (.put "name" name)
+	       (.put "id" (id-factory)))
+	  rtnval (. session save entity  m1)]
+      rtnval)))
 
 
+(defn list-types []
+  (with-hibernate-tx [[session tx] (begin-tx)]
+    (.. session (createQuery "from Type") list)))
