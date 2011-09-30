c21cbc4907927053f0f38a1fdf7011b2309ed93f
Matthew Smith <m0smith@yahoo.com>
2011-09-30 13:36:13 -0600

    hibernate proof of concept work

--- project.clj
+++ project.clj
@@ -10,6 +10,11 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
 		 [com.miglayout/miglayout "3.7.3" :classifier "swing"]
+		 [org.hibernate/hibernate-annotations "3.4.0.GA"]
+		 [org.slf4j/slf4j-api "1.6.2"]
+		 [org.slf4j/slf4j-log4j12 "1.6.2"]
+		 [org.slf4j/log4j-over-slf4j "1.6.2"]
+		 [org.apache.derby/derby "10.1.1.0"]
 		 [clojure-csv/clojure-csv "1.3.2"]
 		 ]
   :dev-dependencies [[swank-clojure "1.3.3-20110809.143608-3"]])
