2466e433d0ca2321aae521bf471466495821800d
Matthew Smith <m0smith@yahoo.com>
2011-10-05 12:11:17 -0600

    finished the log* macro that will allow functions to be created with a level to pass.  The level is the method call

--- src/main/clojure/topoged/log.clj
+++ src/main/clojure/topoged/log.clj
@@ -29,30 +29,28 @@
 (defn ^String space-separated [ args ]
   (apply str (interpose " "  args)))
 
-(defmacro log [logger level objxs]
-  (let [first (gensym "first")
-	rest (gensym "rest")
-	objs (gensym "objs")
-	alogger (gensym "logger")]
-    `(let [~objs ~objxs
+(defmacro log* [logger level objxs]
+  (let [alogger (gensym "logger")]
+    `(let [objs# ~objxs
 	   ~(with-meta alogger {:tag 'org.slf4j.Logger}) ~logger]
-       (if (first ~objs)
-	 (let [[~first & ~rest] ~objs]
-	   (if (instance? java.lang.Throwable ~first)
-	     (. ~alogger ~level (space-separated ~rest) ~first)
-	     (. ~alogger ~level (space-separated ~objs))))
-	 (. ~alogger ~level "")))))
-  
+       (if (first objs#)
+	 (let [[head# & rest#] objs#]
+	   (if (instance? java.lang.Throwable head#)
+	     (let [rtnval# (space-separated rest#)] (. ~alogger ~level rtnval# head#) rtnval#)
+	     (let [rtnval# (space-separated objs#)] (. ~alogger ~level rtnval#) rtnval#)))
+	 (do
+	   (. ~alogger ~level "")
+	   nil )))))
 
 (defn log-error
-  "Designed to be used within with-log.  The log argument is the logger.  If the first objs is a Throwable
+  "Designed to be used within with-log.  The logger argument is the logger.  If the first objs is a Throwable
 it will by passed to the underlying logger.  All the objs will be stringified"
   ([logger & objs]
-     (log logger error objs)))
+     (log* logger error objs)))
 
 (defn log-debug
   "Designed to be used within with-log.  The log argument is the logger.  If the first objs is a Throwable
 it will by passed to the underlying logger.  All the objs will be stringified"
   ([logger & objs]
-     (log logger debug objs)))
+     (log* logger debug objs)))
       
