28b7708bd395044cdea6c140340b72f285326a02
Matthew Smith <m0smith@yahoo.com>
2011-10-05 11:50:00 -0600

    added log macro

--- src/main/clojure/topoged/log.clj
+++ src/main/clojure/topoged/log.clj
@@ -26,15 +26,33 @@
 (defmacro with-named-log [[ name logger] & body]
   `(let [~logger (log-factory ~name)] ~@body ))
 
+(defn ^String space-separated [ args ]
+  (apply str (interpose " "  args)))
+
+(defmacro log [logger level objxs]
+  (let [first (gensym "first")
+	rest (gensym "rest")
+	objs (gensym "objs")
+	alogger (gensym "logger")]
+    `(let [~objs ~objxs
+	   ~(with-meta alogger {:tag 'org.slf4j.Logger}) ~logger]
+       (if (first ~objs)
+	 (let [[~first & ~rest] ~objs]
+	   (if (instance? java.lang.Throwable ~first)
+	     (. ~alogger ~level (space-separated ~rest) ~first)
+	     (. ~alogger ~level (space-separated ~objs))))
+	 (. ~alogger ~level "")))))
+  
 
 (defn log-error
   "Designed to be used within with-log.  The log argument is the logger.  If the first objs is a Throwable
 it will by passed to the underlying logger.  All the objs will be stringified"
-  ([log & objs]
-     (if objs
-       (let [[first & rest] objs]
-	 (if (instance? java.lang.Exception first)
-	   (.error log (apply str (interpose " " rest)) first)
-	   (.error log (apply str (interpose " " objs)))))
-       (.error log ""))))
-       
+  ([logger & objs]
+     (log logger error objs)))
+
+(defn log-debug
+  "Designed to be used within with-log.  The log argument is the logger.  If the first objs is a Throwable
+it will by passed to the underlying logger.  All the objs will be stringified"
+  ([logger & objs]
+     (log logger debug objs)))
+      
