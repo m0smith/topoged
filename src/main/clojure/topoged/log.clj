eba80a451422455097ec3f53a9b27c63dfad7bc3
Matthew Smith <m0smith@yahoo.com>
2011-10-05 08:56:52 -0600

    Added log-error function

--- src/main/clojure/topoged/log.clj
+++ src/main/clojure/topoged/log.clj
@@ -2,6 +2,7 @@
   (:import [org.slf4j LoggerFactory]))
 
 
+
 (def log-factory
      (letfn [(logger [name]
 		     (println "creating logger" name)
@@ -10,3 +11,15 @@
 
 (defmacro with-log [[ logger] & body]
   `(let [~logger (log-factory (str *ns*))] ~@body ))
+
+(defn log-error
+  "Designed to be used within with-log.  The log argument is the logger.  If the first objs is a Throwable
+it will by passed to the underlying logger.  All the objs will be stringified"
+  ([log & objs]
+     (if objs
+       (let [[first & rest] objs]
+	 (if (instance? java.lang.Exception first)
+	   (.error log (apply str (interpose " " rest)) first)
+	   (.error log (apply str (interpose " " objs)))))
+       (.error log ""))))
+       
