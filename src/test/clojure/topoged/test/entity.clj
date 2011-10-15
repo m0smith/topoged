96cff1ead30ff9831d4573ad1e9339f14156440e
Matthew Smith <matt@m0smith.com>
2011-10-14 21:46:54 -0600

    handle HEAD, SUBM and INDI

--- src/test/clojure/topoged/test/entity.clj
+++ src/test/clojure/topoged/test/entity.clj
@@ -1,5 +1,7 @@
 (ns topoged.test.entity
   (:use [clojure.java.io :only [reader writer input-stream output-stream]]
+	[clojure.contrib.seq-utils :only (find-first)]
+	[clojure.pprint :only (pprint)]
 	[topoged.file :only (copy-md5)]
 	[topoged.gedcom :only [gedcom-seq]]))
 
@@ -21,8 +23,18 @@
   )
 
 (defstruct TYPE :TYPE_ID :TYPE_NAME)
+
 (defstruct SOURCE :SOURCE_ID :TYPE_ID)
+(defstruct SOURCE_WITHIN_SOURCE :PARENT_SOURCE_ID :MEMBER_SOURCE_ID :ORDER)
 (defstruct REPRESENTAITON :SOURCE_ID :TYPE_ID :CONTENT :COMMENTS)
+(defstruct REPOSITORY :REPOSITORY_ID :TYPE_ID )
+(defstruct REPOSITORY_SOURCE :REPOSITORY_ID :SOURCE_ID :CALL_NUMBER :DESCRIPTION)
+(defstruct REPOSITORY_ATTRIBUTE :REPOSITORY_ID :ATTRIBUTE_ID)
+
+(defstruct PERSONA :PERSONA_ID)
+(defstruct PERSONA_SOURCE :PERSONA_ID :SOURCE_ID)
+(defstruct PERSONA_ATTRIBUTE :PERSONA_ID :ATTRIBUTE_ID)
+(defstruct ATTRIBUTE :ATTRIBUTE_ID :TYPE_ID :VALUE)
 
 
 (defn update-state [state key value]
@@ -48,24 +60,130 @@
       :type, #{
 	       (struct TYPE "GEDCOM" "GEDCOM")
 	       (struct TYPE "MD5" "MD5")
+	       (struct TYPE "text/plain" "text/plain")
 	       }
-      :source, #{
-		(struct SOURCE "GEDCOM" "GEDCOM")
-		}
+
       })
+
+(defn handler-factory [m]
+  (fn [state rec]
+    (if-let [func (get m (:tag rec))]
+      (func state rec)
+      state)))
+
+(defn match-in-fn [ [& ks] val]
+  #(= val (get-in % ks)))
+
+
+(defn subm-handler [source-zero]
+  (fn [state rec]
+    (let [repo-id (:value rec)
+	  state (-> state
+		    (update-state  :repository
+				   (struct REPOSITORY repo-id "HOME"))
+		    (update-state  :repository_source
+				   (struct REPOSITORY_SOURCE repo-id (:SOURCE_ID source-zero))))]
+      (reduce #(let [line-number (get-in %2 [:attrs :line-number])
+		     rep (get-in %2 [:attrs :representation]) 
+		     tag (:tag %2)
+		     source-id (str "SOURCE" line-number)] 
+		 (-> %1
+		     (update-state :source
+				   (struct SOURCE
+					   source-id
+					   "LINE"))
+		     (update-state :source_within_source
+				   (struct SOURCE_WITHIN_SOURCE
+					   (:SOURCE_ID source-zero)
+					   source-id
+					   line-number))
+		     (update-state :representation
+				   (struct REPRESENTAITON
+					   source-id
+					   "text/plain"
+					   rep))
+		     (update-state :attribute
+				   (struct ATTRIBUTE
+					   (str "ATTR" line-number)
+					   (name tag)
+					   (:value %2)
+					   ))
+		     (update-state :type
+				   (struct TYPE (name tag) (name tag)))
+		     (update-state :repository_attribute
+				   (struct REPOSITORY_ATTRIBUTE repo-id
+					   (str "ATTR" line-number)))))
+	      
+	      state (:content rec)))))
+
+(defn indi-handler [source-zero]
+  (fn [state rec]
+    (let [persona-id (:value rec)
+	  state (-> state
+		    (update-state  :PERSONA
+				   (struct PERSONA persona-id ))
+		    (update-state  :repository_source
+				   (struct PERSONA_SOURCE persona-id (:SOURCE_ID source-zero))))]
+      (reduce #(let [line-number (get-in %2 [:attrs :line-number])
+		     rep (get-in %2 [:attrs :representation]) 
+		     tag (:tag %2)
+		     source-id (str "SOURCE" line-number)] 
+		 (-> %1
+		     (update-state :source
+				   (struct SOURCE
+					   source-id
+					   "LINE"))
+		     (update-state :source_within_source
+				   (struct SOURCE_WITHIN_SOURCE
+					   (:SOURCE_ID source-zero)
+					   source-id
+					   line-number))
+		     (update-state :representation
+				   (struct REPRESENTAITON
+					   source-id
+					   "text/plain"
+					   rep))
+		     (update-state :attribute
+				   (struct ATTRIBUTE
+					   (str "ATTR" line-number)
+					   (name tag)
+					   (:value %2)
+					   ))
+		     (update-state :type
+				   (struct TYPE (name tag) (name tag)))
+		     (update-state :persona_attribute
+				   (struct PERSONA_ATTRIBUTE persona-id
+					   (str "ATTR" line-number)))))
+	      
+	      state (:content rec)))))
 
 (defn process-gedcom [f]
   (let [out-name "/tmp/f.ged"
+	source-zero (struct SOURCE "SOURCE0" "GEDCOM")
 	md5 (let [in  f
 		  out out-name]
 	      (copy-md5 in out))
 	state (merge initial-state
-		     {:representation
+		     { :source,
+		      #{
+			source-zero
+			}
+		      :representation
 		      #{
 			(struct REPRESENTAITON "GEDCOM" "MD5" md5)
 			(struct REPRESENTAITON "GEDCOM" "text/plain" (slurp out-name))
 			}
-		      })]
+		      })
+	handler (handler-factory
+		 {
+		  :HEAD #(merge %1 {:forward
+				    {
+				     (-> (find-first (match-in-fn [:tag] :SUBM) (:content %2) )
+					 :value)
+				     source-zero}})
+		  :SUBM (subm-handler source-zero)
+		  :INDI (indi-handler source-zero)
+		  })]
     (with-open [rdr (reader out-name)]
       (reduce handler state  (gedcom-seq (line-seq rdr))))))
 
