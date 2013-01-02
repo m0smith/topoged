6e8ec9eb5aa0fd438c8a195e8d7c1b86e96d898f
Matthew O. Smith <matt@m0smith.com>
2013-01-02 00:39:42 +0000

    changed the UI to seesaw

--- project.clj
+++ project.clj
@@ -10,6 +10,7 @@
   :main topoged.viewer.frame
   :extra-classpath-dirs [ "/usr/lib/jvm/jdk1.7.0/lib/tools.jar" ]
   :dependencies [[org.clojure/clojure "1.5.0-RC1"]
+                 [seesaw "1.4.2"]
                  [com.ashafa/clutch "0.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.slf4j/slf4j-api "1.6.2"]
--- src/main/clojure/topoged/viewer/frame.clj
+++ src/main/clojure/topoged/viewer/frame.clj
@@ -3,33 +3,28 @@
 	[topoged.viewer.status]
 	[topoged.service.plugin.info]
 	[topoged.plugin.gedcom.import.core :only (gedcom-import-action)]
-	[topoged.gedoverse :only ( log persona-agent)])
-  (:import
-   (java.io File InputStream OutputStream)
-   (javax.swing DefaultListModel JFileChooser JFrame JMenu JMenuBar JMenuItem JLabel JList JPanel JScrollPane ListModel SwingUtilities Timer)
-   (java.awt BorderLayout TextArea Font Color)))
-
-(set! *warn-on-reflection* true)
+	[topoged.gedoverse :only (log persona-agent)]
+        [seesaw core graphics]))
 
   
 (defn display-personas []
   (let [p @persona-agent]
     (sort-by first (map (fn [x] [(:name (val x)) (key x)]) (seq p)))))
 
-(defmacro with-action [component event & body]
-  `(. ~component addActionListener
-      (proxy [java.awt.event.ActionListener] []
-        (actionPerformed [~event] ~@body))))
+;; (defmacro with-action [component event & body]
+;;   `(. ~component addActionListener
+;;       (proxy [java.awt.event.ActionListener] []
+;;         (actionPerformed [~event] ~@body))))
 
-(defmacro with-window-closing [component event & body]
-  `(. ~component addWindowListener
-      (proxy [java.awt.event.WindowAdapter] []
-	(windowClosing  [~event] ~@body))))
+;; (defmacro with-window-closing [component event & body]
+;;   `(. ~component addWindowListener
+;;       (proxy [java.awt.event.WindowAdapter] []
+;; 	(windowClosing  [~event] ~@body))))
 
-(defn add-sub-panel [panel]
-  (let [sub-panel (JPanel.)]
-    (.add panel sub-panel)
-    sub-panel))
+;; (defn add-sub-panel [panel]
+;;   (let [sub-panel (JPanel.)]
+;;     (.add panel sub-panel)
+;;     sub-panel))
 
 
 ;;TextArea text = new TextArea();
@@ -38,45 +33,90 @@
 ;;        text.setForeground(Color.blue);
 ;;        f.add(text, BorderLayout.CENTER);
 
-(defn viewer-app []
-  ""
-  (let [frame  (JFrame. "Topoged Viewer .1")
-        ^JPanel status-info (doto (JPanel.) (.add (JLabel. "Status:")))
-        ^JPanel view-panel  (doto ( JLabel. "Topoged .1")
-                              (.setFont (Font. "Serif" Font/ITALIC, 72))
-                              (.setForeground Color/BLUE))
-	close-window (fn []  (.dispose frame))
-	plugin-info (create-plugin-info frame view-panel)]
+;; (defn viewer-app []
+;;   ""
+;;   (let [frame  (JFrame. "Topoged Viewer .1")
+;;         ^JPanel status-info (doto (JPanel.) (.add (JLabel. "Status:")))
+;;         ^JPanel view-panel  (doto ( JLabel. "Topoged .1")
+;;                               (.setFont (Font. "Serif" Font/ITALIC, 72))
+;;                               (.setForeground Color/BLUE))
+;; 	close-window (fn []  (.dispose frame))
+;; 	plugin-info (create-plugin-info frame view-panel)]
 
-    (with-window-closing frame _ (close-window))
+;;     (with-window-closing frame _ (close-window))
 	
-    (doto frame
-      (.setContentPane
-       (doto (JPanel.)
-         (.setSize 400 600)
-         (.setLayout (BorderLayout.))
-         (.add view-panel BorderLayout/CENTER)
-         (.add (doto (JPanel.)
-                 (.add status-info))
-               BorderLayout/SOUTH)))
-      (.setJMenuBar
-       (doto (JMenuBar.)
-         (.add (doto (JMenu. "File")
-                 (.add (doto (JMenuItem. "Import")
-                         (with-action _
-                           (let [f (gedcom-import-action (assoc plugin-info :status (add-sub-panel status-info)))]
-                             (println @f)))))
+;;     (doto frame
+;;       (.setContentPane
+;;        (doto (JPanel.)
+;;          (.setSize 400 600)
+;;          (.setLayout (BorderLayout.))
+;;          (.add view-panel BorderLayout/CENTER)
+;;          (.add (doto (JPanel.)
+;;                  (.add status-info))
+;;                BorderLayout/SOUTH)))
+;;       (.setJMenuBar
+;;        (doto (JMenuBar.)
+;;          (.add (doto (JMenu. "File")
+;;                  (.add (doto (JMenuItem. "Import")
+;;                          (with-action _
+;;                            (let [f (gedcom-import-action (assoc plugin-info :status (add-sub-panel status-info)))]
+;;                              (println @f)))))
                  
-                 (.add (doto (JMenuItem. "Exit")
-                         (with-action _ (close-window))))))))
+;;                  (.add (doto (JMenuItem. "Exit")
+;;                          (with-action _ (close-window))))))))
       
-      (.setBounds 300 300 600 800)
-      (.setSize 400 600)
-      (.pack)
-      (.setDefaultCloseOperation (JFrame/EXIT_ON_CLOSE))
-      (.setVisible true))))
+;;       (.setBounds 300 300 600 800)
+;;       (.setSize 400 600)
+;;       (.pack)
+;;       (.setDefaultCloseOperation (JFrame/EXIT_ON_CLOSE))
+;;       (.setVisible true))))
 
-(defn -main [] ( viewer-app))
+
+(def status-bar (label :text "STATUS" :h-text-position :center))
+
+(def top-frame (border-panel
+                :size [500 :by 500]
+                :center (left-right-split
+                         (scrollable (listbox :model ["Foo" "Bar" "Yum"]))
+                         "PEDIGREE")
+                :north (label :text "TOPOGED" :h-text-position :center)
+                :south status-bar))
+
+(defn a-import-gedcom-handler [e]
+  (let [pi (create-plugin-info top-frame top-frame)]
+    (println pi)
+    (gedcom-import-action (assoc pi :status status-bar))
+    (println (display-personas))))
+
+(defn a-settings-handler [e]
+  (println e))
+
+(def file-menu
+  (menu :text "File"
+        :items
+        [
+         (action :handler a-import-gedcom-handler
+                 :name "Import GEDCOM"
+                 :tip "Import a GEDCOM file.")
+         (action :handler a-settings-handler
+                 :name "Settings"
+                 :tip "Setting for TOPOGED.")
+         ]))
+
+(defn viewer-app []
+  (-> (frame :title "Topoged",
+             :size [500 :by 500]
+             :content top-frame
+             :menubar (menubar
+                       :items [file-menu
+                               (menu :text "Reports" :items [])
+                               (menu :text "Tasks" :items [])])
+             :on-close :exit)
+      pack!
+      show!))
+
+(defn -main []
+  (invoke-later (viewer-app)))
 
 ;;(javax.swing.SwingUtilities/invokeLater topoged.viewer.frame/viewer-app)
 
