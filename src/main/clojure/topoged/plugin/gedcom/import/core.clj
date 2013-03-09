b5f5b29a7c42fb1d086305cc1f36936e5417f418
Matthew O. Smith <matt@m0smith.com>
2013-03-09 08:25:56 -0700

    Added well-known types for GEDCOM imports

--- src/main/clojure/topoged/plugin/gedcom/import/core.clj
+++ src/main/clojure/topoged/plugin/gedcom/import/core.clj
@@ -14,6 +14,44 @@
 
 ;;(set! *warn-on-reflection* true)
 
+(def gedcom-well-known-types
+  [
+   [:gedcom-dest #uuid e25ad0b0-88ca-11e2-9e96-0800200c9a66 "GEDCOM A system receiving data"
+    "GEDCOM Destination" #uuid "c8bfb535-9bcb-40ad-b896-b635275722ad"]
+   [:gedcom-source-version #uuid e25ad0b1-88ca-11e2-9e96-0800200c9a66 "GEDCOM SOURCE Indicates which version of a product, item, or publication is being used or referenced"
+    "GEDCOM Source Version" #uuid "eb1208de-a1dd-49f5-a744-5c14f2b955cd"]
+   [:gedcom-source-name #uuid e25ad0b2-88ca-11e2-9e96-0800200c9a66 "GEDCOM SOURCE NAME  A word or combination of words used to help identify an individual, title, or other item. More than one NAME line should be used for people who were known by multiple names"
+    "GEDCOM Source Name" #uuid "3baa1b76-8b9e-479c-aa22-c6b0258f1fa2"]
+   [:gedcom-source-corp #uuid e25ad0b3-88ca-11e2-9e96-0800200c9a66 "GEDCOM SOURCE A name of an institution, agency, corporation, or company"
+    "GEDCOM Source Corp" #uuid "5c6aa697-e5d8-4b9b-8b98-bbd790be2eec"]
+   [:gedcom-source-address #uuid e25ad0b4-88ca-11e2-9e96-0800200c9a66 "GEDCOM SOURCE The contemporary place, usually required for postal purposes, of an individual, a submitter of information, a repository, a business, a school, or a company."
+    "GEDCOM Source Address" #uuid "56603543-7495-4bc0-adf4-b6d241ab6267"]
+   [:gedcom-version #uuid e25ad0b5-88ca-11e2-9e96-0800200c9a66 "GEDCOM Indicates which version of a product, item, or publication is being used or referenced"
+    "GEDCOM Version" #uuid "98a5985e-cfc7-43bd-b013-12fde983121d"]
+   [:gedcom-form #uuid e25ad0b6-88ca-11e2-9e96-0800200c9a66 "GEDCOM An assigned name given to a consistent format in which information can be conveyed"
+    "GEDCOM Form" #uuid "0f35ce96-f1da-497a-82c4-442b8b3422fd"]
+   [:gedcom-source #uuid e25ad0b7-88ca-11e2-9e96-0800200c9a66 "GEDCOM SOURCE The initial or original material from which information was obtained."
+    "GEDCOM Source" #uuid "bbf61d6e-7228-4969-bedc-c3d98e9a7280"]
+   ])
+;
+;#uuid e25ad0b8-88ca-11e2-9e96-0800200c9a66
+;#uuid e25ad0b9-88ca-11e2-9e96-0800200c9a66
+;#uuid e25ad0ba-88ca-11e2-9e96-0800200c9a66
+;#uuid e25ad0bb-88ca-11e2-9e96-0800200c9a66
+;#uuid e25ad0bc-88ca-11e2-9e96-0800200c9a66
+;#uuid e25ad0bd-88ca-11e2-9e96-0800200c9a66
+;#uuid e25ad0be-88ca-11e2-9e96-0800200c9a66
+;#uuid e25ad0bf-88ca-11e2-9e96-0800200c9a66
+;#uuid e25ad0c0-88ca-11e2-9e96-0800200c9a66
+;#uuid e25ad0c1-88ca-11e2-9e96-0800200c9a66
+;#uuid e25ad0c2-88ca-11e2-9e96-0800200c9a66
+;#uuid e25ad0c3-88ca-11e2-9e96-0800200c9a66
+;#uuid e25ad0c4-88ca-11e2-9e96-0800200c9a66
+;#uuid e25ad0c5-88ca-11e2-9e96-0800200c9a66
+;#uuid e25ad0c6-88ca-11e2-9e96-0800200c9a66
+;#uuid e25ad0c7-88ca-11e2-9e96-0800200c9a66
+;#uuid e25ad0c8-88ca-11e2-9e96-0800200c9a66
+
 (defmulti handle-record (fn [_ __ rec] (:tag rec))) 
 
 (remove-method handle-record :FAM)
