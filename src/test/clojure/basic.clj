a2b8e3fe39c6142cf4f6d58ff12d42f2606da712
Matthew O. Smith <matt@m0smith.com>
2014-03-25 08:45:14 -0600

    Remove lines moved to path.clj

--- src/test/clojure/basic.clj
+++ src/test/clojure/basic.clj
@@ -8,24 +8,6 @@
            [topoged.db :as tdb]))
 
 
-(defn add-parents [db child father mother order]
-  (when child
-    (let [template [child {:order order} {} {:order 0} father]
-          [child e1 birth _ _] (if father
-                                 (path-create db child->parent-path template)
-                                 template)]
-      (when mother
-        (path-create db child->parent-path 
-                     [child e1 birth {:order 1} mother])))))
-
-(defn parents-of [node]
-  (let [m (reduce conj {}
-                  (for [[_ _ _ edge parent] (path-query child->parent-path node)]
-                    [(v/get edge :order) parent]))]
-    [(get m 0) (get m 1)]))
-
-(defn add-spouse [db s1 s2 ]
-  (path-create db marriage-path [ s1 {:order 0} {} {:order 1} s2]))
 
 
 (def p 
